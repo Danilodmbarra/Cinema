fbcbb1b07123f1f848068139a153d0a767475944
Danilodmbarra <47570163+Danilodmbarra@users.noreply.github.com>
2019-05-30 16:00:48 -0300

    Cinema_Part1Review
    Segundo commit
    Dividido em classes - Login, Menu
    
    Login responsavel por logar e autentificar o usuario
    
    Menu responsavel por abrir de acordo com o grau de permissao do usuario
    
    Metodos --> Explica o que exatamente ele faz ---> ex:AberturaDeMenus,MenuAdministracao,TelaDeAbertura.
    
    nome dos Arrays modificados -->sArrayLoginDeUsuarios -->  s= tipo do array = String, Array, o que o Array contem = LoginDeUsuarios
    adicionado array de bloquiados

--- Trabalho_final/Program.cs
+++ Trabalho_final/Program.cs
@@ -8,11 +8,15 @@ namespace CineTech
 {
     class Menu
     {
-        public void AberturaDeMenus(int pAutentificacao, string pUsuario,string pSenha,string []spArrayDeLoginDeUsuarios,string[] spArrayDeSenhaDeUsuarios,int pTentativasDeLogin, string[] spArrayDeUsuariosBloquiados,ref int posicao)
+        public void AberturaDeMenus(int pAutentificacao, string pUsuario, string pSenha, string[] spArrayDeLoginDeUsuarios, string[] spArrayDeSenhaDeUsuarios, int pTentativasDeLogin, string[] spArrayDeUsuariosBloquiados, ref int posicao)
         {
             if (pAutentificacao == 1)
             {
-                MenuAdministracao(pUsuario,pSenha,spArrayDeLoginDeUsuarios,spArrayDeSenhaDeUsuarios,pTentativasDeLogin,spArrayDeUsuariosBloquiados, ref posicao);
+                MenuAdministracao(pUsuario, pSenha, spArrayDeLoginDeUsuarios, spArrayDeSenhaDeUsuarios, pTentativasDeLogin, spArrayDeUsuariosBloquiados, ref posicao);
+            }
+            else if(pAutentificacao == 2)
+             {
+                MenuFuncionarios(pUsuario, pSenha, spArrayDeLoginDeUsuarios, spArrayDeSenhaDeUsuarios, pTentativasDeLogin, spArrayDeUsuariosBloquiados, ref posicao);
             }
         }
         public void MenuAdministracao(string pUsuario, string pSenha, string[] spArrayDeLoginDeUsuarios, string[] spArrayDeSenhaDeUsuarios, int pTentativasDeLogin, string[] spArrayDeUsuariosBloquiados, ref int posicao)
@@ -47,8 +51,8 @@ namespace CineTech
                         }
                     case ConsoleKey.F4:
                         {
-                            MetodosDeInicializacao.TelaAbertura();
-                            MetodosDeInicializacao.TelaCarregamento();
+                            MetodosDeInicializacao.TelaDeAbertura();
+                            MetodosDeInicializacao.TelaDeCarregamento();
                             MetodosDeInicializacao.MenuLogin(spArrayDeLoginDeUsuarios, spArrayDeSenhaDeUsuarios, spArrayDeUsuariosBloquiados, pTentativasDeLogin, ref posicao);
                             condicao = false;
 
@@ -66,18 +70,93 @@ namespace CineTech
                         }
                 }
             } while (condicao);
-            MetodosDeInicializacao.TelaAbertura();
-            MetodosDeInicializacao.TelaCarregamento();
+            MetodosDeInicializacao.TelaDeAbertura();
+            MetodosDeInicializacao.TelaDeCarregamento();
             MetodosDeInicializacao.MenuLogin(spArrayDeLoginDeUsuarios, spArrayDeSenhaDeUsuarios, spArrayDeUsuariosBloquiados, pTentativasDeLogin, ref posicao);
-
         }
+        public void MenuFuncionarios(string pUsuario, string pSenha, string[] spArrayDeLoginDeUsuarios, string[] spArrayDeSenhaDeUsuarios, int pTentativasDeLogin, string[] spArrayDeUsuariosBloquiados, ref int posicao)
+        {
+            Login MetodosDeInicializacao = new Login();
+            ConsoleKeyInfo lertecla;
+            bool condicao = true;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("==========================================================================");
+                Console.WriteLine("                           ÁREA DO FUNCIONÁRIO                            ");
+                Console.WriteLine("==========================================================================");
+                Console.WriteLine("Bem vindo, {0}! Pressione a tecla referente à opção desejada: ", pUsuario);
+                Console.WriteLine();
+                Console.WriteLine("F1) Gestão de Clientes");
+                Console.WriteLine();
+                Console.WriteLine("F2) Gestão Financeira");
+                Console.WriteLine();
+                Console.WriteLine("F3) Gestão de Produtos");
+                Console.WriteLine();
+                Console.WriteLine("F4) Retornar à tela de Login");
+                lertecla = Console.ReadKey();
 
+                switch (lertecla.Key)
+                {
+                    case ConsoleKey.F1:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("==========================================================================");
+                            Console.WriteLine("                 ÁREA DO FUNCIONÁRIO - GESTÃO DE CLIENTES                 ");
+                            Console.WriteLine("==========================================================================");
+                            Console.WriteLine("Método em desenvolvimento! Pressione qualquer tecla para retornar ao menu.");
+                            Console.ReadKey();
+                            break;
+                        }
+                    case ConsoleKey.F2:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("==========================================================================");
+                            Console.WriteLine("                 ÁREA DO FUNCIONÁRIO - GESTÃO FINANCEIRA                  ");
+                            Console.WriteLine("==========================================================================");
+                            Console.WriteLine("Método em desenvolvimento! Pressione qualquer tecla para retornar ao menu.");
+                            Console.ReadKey();
+                            break;
+                        }
+                    case ConsoleKey.F3:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("==========================================================================");
+                            Console.WriteLine("                 ÁREA DO FUNCIONÁRIO - GESTÃO DE PRODUTOS                 ");
+                            Console.WriteLine("==========================================================================");
+                            Console.WriteLine("Método em desenvolvimento! Pressione qualquer tecla para retornar ao menu.");
+                            Console.ReadKey();
+                            break;
+                        }
+                    case ConsoleKey.F4:
+                        {
+                            condicao = false;
+                            break;
+                        }
+                    default:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("==========================================================================");
+                            Console.WriteLine("                           ÁREA DO FUNCIONÁRIO                            ");
+                            Console.WriteLine("==========================================================================");
+                            Console.WriteLine("Opção Inválida. Pressione qualquer tecla para retornar ao menu.");
+                            Console.ReadKey();
+                            break;
+                        }
+                }
+            } while (condicao);
 
+            MetodosDeInicializacao.TelaDeAbertura();
+            MetodosDeInicializacao.TelaDeCarregamento();
+            MetodosDeInicializacao.MenuLogin(spArrayDeLoginDeUsuarios, spArrayDeSenhaDeUsuarios, spArrayDeUsuariosBloquiados, pTentativasDeLogin, ref posicao);
+        }
     }
+
+
     class Login
     {
 
-        public void TelaAbertura()
+        public void TelaDeAbertura()
         {
             Console.WriteLine("  ______  __  .__   __.  _______ .___________. _______   ______  __    __  ");
             Console.WriteLine(" /      ||  | |  \" |  | |   ____||           ||   ____| /      ||  |  |  | ");
@@ -90,20 +169,21 @@ namespace CineTech
 
         }
 
-        public void TelaCarregamento()
+        public void TelaDeCarregamento()
         {
             Console.WriteLine("Loading");
             for (int i = 0; i < 74; i++)
             {
                 Console.Write("|");
                 System.Threading.Thread.Sleep(30);
-                
+
             }
             Console.Clear();
         }
 
-        public void MenuLogin(string[] spArrayDeLoginDeUsuarios, string[] spArrayDeSenhaDeUsuarios, string[] spArrayDeUsuariosBloquiados, int pTentativasDeLogin, ref int posicaoBloquiados)
+        public void MenuLogin(string[] spArrayDeLoginDeUsuarios, string[] spArrayDeSenhaDeUsuarios, string[] sArrayDeUsuariosBloquiados, int pTentativasDeLogin, ref int posicaoBloquiados)
         {
+
             string usuario, senha;
             int valorAutentificao;
             Menu MetodosDoMenu = new Menu();
@@ -118,34 +198,38 @@ namespace CineTech
             usuario = Console.ReadLine();
             Console.Write("SENHA: ");
             senha = Console.ReadLine();
-            valorAutentificao = Autentificacao(usuario, senha, spArrayDeLoginDeUsuarios, spArrayDeSenhaDeUsuarios, pTentativasDeLogin, spArrayDeUsuariosBloquiados);
+            valorAutentificao = Autentificacao(usuario, senha, spArrayDeLoginDeUsuarios, spArrayDeSenhaDeUsuarios, pTentativasDeLogin, sArrayDeUsuariosBloquiados);
 
             if (valorAutentificao == 0)
             {
+
                 Console.WriteLine("{0} Usuario esta bloquiado contate o administrador", usuario);
+                MetodosDoMenu.AberturaDeMenus(valorAutentificao, usuario, senha, spArrayDeLoginDeUsuarios, spArrayDeSenhaDeUsuarios, pTentativasDeLogin, sArrayDeUsuariosBloquiados, ref posicaoBloquiados);
             }
             if (valorAutentificao == 1 || (valorAutentificao == 2))
             {
-                MetodosDoMenu.AberturaDeMenus(valorAutentificao, usuario, senha, spArrayDeLoginDeUsuarios, spArrayDeSenhaDeUsuarios, pTentativasDeLogin, spArrayDeUsuariosBloquiados, ref posicaoBloquiados);
+                MetodosDoMenu.AberturaDeMenus(valorAutentificao, usuario, senha, spArrayDeLoginDeUsuarios, spArrayDeSenhaDeUsuarios, pTentativasDeLogin, sArrayDeUsuariosBloquiados, ref posicaoBloquiados);
             }
 
             else if (valorAutentificao == 3)
             {
                 --pTentativasDeLogin;
-                Console.Clear();
+
                 Console.WriteLine("Senha incorreta Tente novamente");
                 Console.WriteLine("Tentativas {0} de 3", pTentativasDeLogin);
 
-                MenuLogin(spArrayDeLoginDeUsuarios, spArrayDeSenhaDeUsuarios, spArrayDeUsuariosBloquiados, pTentativasDeLogin, ref posicaoBloquiados);
+                MenuLogin(spArrayDeLoginDeUsuarios, spArrayDeSenhaDeUsuarios, sArrayDeUsuariosBloquiados, pTentativasDeLogin, ref posicaoBloquiados);
                 Console.ReadKey();
 
             }
             else if (valorAutentificao == 4)
             {
-                spArrayDeUsuariosBloquiados[posicaoBloquiados] = usuario;
+
+                sArrayDeUsuariosBloquiados[posicaoBloquiados] = usuario;
                 Console.WriteLine("Usuario Bloquiado");
                 Console.ReadKey();
                 posicaoBloquiados++;
+                MenuLogin(spArrayDeLoginDeUsuarios, spArrayDeSenhaDeUsuarios, sArrayDeUsuariosBloquiados, pTentativasDeLogin, ref posicaoBloquiados);
             }
             else
             {
@@ -155,63 +239,63 @@ namespace CineTech
 
             Console.ReadKey();
         }
-        }
-
-    public int Autentificacao(string pUsuario, string pSenha, string[] pArrayDeLoginDeUsuarios, string[] psArrayDeUsuariosSenha, int pTentativasDeLogin, string[] spArrayDeUsuariosBloquiados)
-    {
-
-        string admin = "Administrador";
-        string adminSenha = "123";
-        int posicao, ternarioTentativasDeLogin;
-
-        
-
-        if (pUsuario == admin && pSenha == adminSenha)
+        public int Autentificacao(string pUsuario, string pSenha, string[] pArrayDeLoginDeUsuarios, string[] psArrayDeUsuariosSenha, int pTentativasDeLogin, string[] spArrayDeUsuariosBloquiados)
         {
-            return 1;
 
-        }
-        else if (spArrayDeUsuariosBloquiados.Contains(pUsuario))
-        {
-            return 0;
-        }
-        else if (pArrayDeLoginDeUsuarios.Contains(pUsuario))
-        {
-            posicao = Array.IndexOf(pArrayDeLoginDeUsuarios, pUsuario);
-            if (psArrayDeUsuariosSenha[posicao] == pSenha)
+            string admin = "Administrador";
+            string adminSenha = "123";
+            int posicao, ternarioTentativasDeLogin;
+
+
+
+            if (pUsuario == admin && pSenha == adminSenha)
             {
-                return 2;
+                return 1;
+
+            }
+            else if (spArrayDeUsuariosBloquiados.Contains(pUsuario))
+            {
+                return 0;
+            }
+            else if (pArrayDeLoginDeUsuarios.Contains(pUsuario))
+            {
+                posicao = Array.IndexOf(pArrayDeLoginDeUsuarios, pUsuario);
+                if (psArrayDeUsuariosSenha[posicao] == pSenha)
+                {
+                    return 2;
+
+                }
+                else
+                {
+                    return ternarioTentativasDeLogin = (pTentativasDeLogin > 0) ? 3 : 4;
+
+                }
 
             }
             else
             {
-                return ternarioTentativasDeLogin = (pTentativasDeLogin > 0) ? 3 : 4;
-
-            }
-        }
-            
-    }           
-        
-        class Program
-        {
-            static void Main(string[] args)
-            {
-
-                string[] sArrayDeLoginDeUsuarios = { "Danilo", "Leonardo", "Joao" };
-                string[] sArrayDeSenhaDeUsuarios = { "1234", "1111", "1234" };
-                string[] sArrayDeUsuariosBloquiados = new string[100];
-                int tentativasDeLogin = 3, posicaoBloquiados = 0;
-
-                Login MetodosDeInicializacao = new Login();
-                MetodosDeInicializacao.TelaAbertura();
-                MetodosDeInicializacao.TelaCarregamento();
-                MetodosDeInicializacao.MenuLogin(sArrayDeLoginDeUsuarios, sArrayDeSenhaDeUsuarios, sArrayDeUsuariosBloquiados, tentativasDeLogin, ref posicaoBloquiados);
-
-
-
-
+                return 5;
             }
         }
     }
 
+    class Program
+    {
+        static void Main(string[] args)
+        {
 
+            string[] sArrayDeLoginDeUsuarios = { "Danilo", "Leonardo", "Joao" };
+            string[] sArrayDeSenhaDeUsuarios = { "1234", "1111", "1234" };
+            string[] sArrayDeUsuariosBloquiados = new string[100];
+            int tentativasDeLogin = 3, posicaoBloquiados = 0;
+
+            Login MetodosDeInicializacao = new Login();
+            MetodosDeInicializacao.TelaDeAbertura();
+            MetodosDeInicializacao.TelaDeCarregamento();
+            MetodosDeInicializacao.MenuLogin(sArrayDeLoginDeUsuarios, sArrayDeSenhaDeUsuarios, sArrayDeUsuariosBloquiados, tentativasDeLogin, ref posicaoBloquiados);
+
+
+        }
+    }
+
+}
